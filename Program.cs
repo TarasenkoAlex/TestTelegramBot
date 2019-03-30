7e212a3df407e678e602f14fce675fa689e19fba
Alex Tarasenko <a.tarasenko@intecom-energo.ru>
2019-03-31 01:25:09 +0300

    Добавил тестовый проект. #1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace TestTelegramBot
+{
+    static class Progёram
+    {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("835831458:AAGqMWSC1nUHcwWn4wVxqrsEBbyFJ7vvtE8");
+        static void Main(string[] args)
+        {
+            Bot.OnMessage += Bot_OnMessage;
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Type == MessageType.Text)
+            {
+                Console.WriteLine(e.Message.Text);
+            }
+        }
+    }
+}
